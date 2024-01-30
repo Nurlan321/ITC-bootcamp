41ae6cdb719bb0c1a200197349b08cacd8ee1b89
User <user@MacBook-Pro-User.local>
2024-01-30 14:01:52 +0600

    domashka

---
+++ ritm.py
@@ -0,0 +1,28 @@
+a = 45
+if 0 <= a <= 21 or 57 <= a <= 100:
+    print('a razreshonom')
+else:
+    print("a zapreshonom")
+
+
+a = 40
+
+if 40  % 2 == 0:
+    print('4chet')
+else:
+    print("ne4het")
+if 40 % 3 == 0:
+    print('deli')
+else:
+    print("nedeli")
+if 40 **2 > 1000:
+   print('kfadrat bolishe 1000')
+else:
+    print(" kfadrat menishe 1000")
+
+a = 14
+if a <= 13 or a >= 15:
+    print('a <= 13')
+else:
+    print("a >= 13")
+22
