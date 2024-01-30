786d67518bb91b5f9326ac2c6fae9c5397c3d84d
User <user@MacBook-Pro-User.local>
2024-01-30 14:08:01 +0600

    domashka

---
+++ algeb.py
@@ -0,0 +1,29 @@
+y = 100
+x = 90
+
+print(y)
+print(x)
+
+print(y - 9)
+print(x - 7)
+
+print(y * 8)
+print(x * 6)
+
+print(y <= x)
+print(y >= x)
+
+print(x // 4)
+print(y // 8)
+
+print(x ** 0)
+print(y ** 2)
+
+print(2.3 % 5)
+print(3.1 % 2.3)
+
+print(y != x)
+
+print(2.7 % 8.9)
+print(7.5 / 9.5)
+
