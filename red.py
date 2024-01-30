e6decf01ddcbd36167841343921cdcb21950e0ce
User <user@MacBook-Pro-User.local>
2024-01-30 14:01:20 +0600

    domashka

---
+++ red.py
@@ -0,0 +1,16 @@
+a = 17391
+b = 546
+c = 934
+if (a % 17) < (b % 17) and (a % 17) < (c % 17):
+    print('a % 17 ostatok meshe')
+elif (b % 17) < (a % 17) and (b % 17) < (c % 17):
+    print("b % 17 ostatok meshehe")
+else:
+    print("c % 17 osnanok meshe")
+
+
+x = 13
+a = 172
+if (x ** 2) <= a:
+    print(x**4)
+
