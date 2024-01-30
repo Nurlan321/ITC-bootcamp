fbf56e798f061a6ea79bc77340f5b6d9b167e793
User <user@MacBook-Pro-User.local>
2024-01-30 13:56:32 +0600

    domashka

---
+++ kkd.py
@@ -0,0 +1,38 @@
+q = 20 
+b = 30 
+
+print(q)
+print(b)
+
+print(q - 2)
+print(b - 2)
+
+print(q + 3)
+print(b + 3)
+
+print(q * 2)
+print(b * 2)
+
+print(q / 2)
+print(b / 2)
+
+print(q // 4)
+print(b // 4)
+
+print(33 % 5)
+print(22 % 5)
+print(24 % 3)
+
+print(5 ** 2)
+print(2 ** 1)
+
+
+print(q < b)
+print(q > b)
+
+print(q == b)
+print(20 == 21)
+
+print(q <=  b)
+print(b >= q)
+
