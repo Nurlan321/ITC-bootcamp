c3421d0aa1ddebb7eea5fd39f896bb34085982ba
User <user@MacBook-Pro-User.local>
2024-01-30 14:04:30 +0600

    domashka

---
+++ list.py
@@ -0,0 +1,136 @@
+y = 2**3
+x = 3**2
+if x >= y:
+   print('x>y')
+
+else:
+    print("x>y")
+
+x = 60
+if 0 <= x <= 10 or 70 <= x <= 100:
+    print('ok')
+else:
+    print('a ok')
+
+
+
+x = 22
+
+if 22 % 2 == 0:
+    print('da')
+else:
+    print('net')
+if 22 % 3 == 0:
+    print('tupe1')
+else:
+    print('no')
+if 22 **2 > 100:
+   print('tupe2 100')
+else:
+    print("tupe3 100")
+
+x = 15
+if x <= 12 or x >= 15:
+    print('x <= 12')
+else:
+    print("x >= 12")
+
+
+
+
+x = 100 // 50
+y = 100 / 55
+if x == y:
+    print('yes')
+else:
+    print("no")
+
+number = -100
+if number < 0:
+   print('kakby da')
+else:
+    print("kakby net")
+
+x = 100
+y = 50 
+if x > 0 and y > 100:
+    print('edinica')
+else:
+    print("ne")
+
+
+
+
+x = 10
+y = 5
+if x > y:
+    print('x + 40')
+else:
+    print("y + 22")
+
+x = 1234
+if  x > 22 and x < 51:
+    print('tonn')
+else:
+    print("ves")
+
+age = 15
+if age >= 12:
+    print('litr')
+elif age <= 6:
+    print("gramm")
+else:
+    print("kg")
+
+
+
+
+x =  50
+y = 60
+if x % y == 0:
+    print('da')
+else:
+    print("net")
+
+kg = 200
+gram = 300
+if kg == gram:
+    print('chistiy ves')
+elif kg > gram:
+    print("jstavshiysia ves")
+else:
+    print("budet")
+
+
+
+
+x = 1000
+y = 500
+b = 300
+if x > y and x > b:
+    print(x)
+elif b > a and b > c:
+    print(y)
+else:
+    print(b)
+if x < y and x < b:
+    print(a)
+elif y < x and y < b:
+    print(y)
+else:
+    print(b)
+
+
+
+
+x = 1000
+y = 500
+b = 200
+if (x % 70) < (y % 100) and (x % 70) > (b % 10):
+    print('x % 70')
+elif (y % 70) < (x % 100) and (b % 10) < (y % 100):
+    print("y % 70")
+else:
+    print("b % 70 procent")
+
+
