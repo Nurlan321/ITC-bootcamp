cb42ad73ccfb1791fe729a7e842e5147a0cfbc3b
User <user@MacBook-Pro-User.local>
2024-03-29 22:39:07 +0600

    botmi.py

---
+++ botmi.py
@@ -0,0 +1,39 @@
+from dopbot import TOKEN
+from main import variant_game
+import telebot 
+from telebot import types
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=["start"])
+
+def start(message):
+    menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    b1= types.KeyboardButton("🪨")
+    b2 = types.KeyboardButton("✂️")
+    b3 = types.KeyboardButton("📄")
+    menu.add(b1,b2,b3)
+    bot.send_message(message.chat.id,"Давай поиграем, выбери нужный вариант",reply_markup=menu)
+
+@bot.message_handler(content_types=['text'])
+
+
+def second(message):
+    if message.chat.type == "private":
+        if message.text == "🪨":
+            result = variant_game(1)
+            bot.send_message(message.chat.id,result)
+        elif message.text == "✂️":
+            result = variant_game(2)
+            bot.send_message(message.chat.id,result)
+        elif message.text == "📄":
+            result = variant_game(3)
+            bot.send_message(message.chat.id,result)
+        else:
+            bot.send_message(message.chat.id,"Выберите корретный смайлик")
+
+
+
+
+bot.polling(non_stop=True)
+    
